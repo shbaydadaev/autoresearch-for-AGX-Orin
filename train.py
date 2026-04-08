433ee393d2592bba08802bdb21219cad0fd21a43
shbaydadaev <sh.baydadaev@inha.edu>
2026-04-09 02:12:53 +0900

    Increase output softcap to 30

--- train.py
+++ train.py
@@ -468,7 +468,7 @@ class GPT(nn.Module):
             x = block(x, ve, cos_sin, self.window_sizes[i])
         x = norm(x)
 
-        softcap = 15
+        softcap = 30
         logits = self.lm_head(x)
         logits = logits.float()
         logits = softcap * torch.tanh(logits / softcap)
